a78941d115b3865a05af140779d74b0e9ba8777a
Ernesto Orozco <eroz.bst@gmail.com>
2018-01-05 21:20:27 -0700

    upload

---
+++ MonkeyCall/AppDelegate.cs
@@ -0,0 +1,116 @@
+﻿using Foundation;
+using UIKit;
+using Intents;
+using System;
+
+
+namespace MonkeyCall
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        // class-level declarations
+        #region Constructors
+        public ActiveCallManager CallManager { get; set; }
+        public ProviderDelegate CallProviderDelegate { get; set; }
+        #endregion
+
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+
+            // Code to start the Xamarin Test Cloud Agent
+#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+#endif
+
+            // Initialize the call handlers
+            CallManager = new ActiveCallManager();
+            CallProviderDelegate = new ProviderDelegate(CallManager);
+
+            return true;
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            // Get handle from url
+            var handle = StartCallRequest.CallHandleFromURL(url);
+
+            // Found?
+            if(handle == null){
+                // No, report to system
+                Console.WriteLine("Unable to get call handle from URL: {0}", url);
+                return false;
+            }else{
+                // Yes, start call and inform system
+                CallManager.StartCall(handle);
+                return true;
+            }
+
+            // return base.OpenUrl(app, url, options);
+        }
+
+        public override bool ContinueUserActivity(UIApplication application, NSUserActivity userActivity, UIApplicationRestorationHandler completionHandler)
+        {
+            var handle = StartCallRequest.CallHandleFromActivity(userActivity);
+
+            // Found?
+            if (handle == null)
+            {
+                // No, report to system
+                Console.WriteLine("Unable to get call handle from User Activity: {0}", userActivity);
+                return false;
+            }
+            else
+            {
+                // Yes, start call and inform system
+                CallManager.StartCall(handle);
+                return true;
+            }
+            // return base.ContinueUserActivity(application, userActivity, completionHandler);
+        }
+
+
+
+        public override void OnResignActivation(UIApplication application)
+        {
+            // Invoked when the application is about to move from active to inactive state.
+            // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+            // or when the user quits the application and it begins the transition to the background state.
+            // Games should use this method to pause the game.
+        }
+
+        public override void DidEnterBackground(UIApplication application)
+        {
+            // Use this method to release shared resources, save user data, invalidate timers and store the application state.
+            // If your application supports background exection this method is called instead of WillTerminate when the user quits.
+        }
+
+        public override void WillEnterForeground(UIApplication application)
+        {
+            // Called as part of the transiton from background to active state.
+            // Here you can undo many of the changes made on entering the background.
+        }
+
+        public override void OnActivated(UIApplication application)
+        {
+            // Restart any tasks that were paused (or not yet started) while the application was inactive. 
+            // If the application was previously in the background, optionally refresh the user interface.
+        }
+
+        public override void WillTerminate(UIApplication application)
+        {
+            // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
+        }
+    }
+}
+
---
+++ MonkeyCall/CallKit/ActiveCall.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using CoreFoundation;
+using Foundation;
+
+
+namespace MonkeyCall
+{
+    public class ActiveCall
+    {
+        #region Private Variables
+        private bool isConnecting;
+        private bool isConnected;
+        private bool isOnhold;
+        #endregion
+
+        #region Computed Propierties
+        public NSUuid UUID { get; set; }
+        public bool IsOutgoing { get; set; }
+        public string Handle { get; set; }
+        public DateTime StartedConnectingOn { get; set; }
+        public DateTime ConnectedOn { get; set; }
+        public DateTime EndedOn { get; set; }
+        public bool IsConnecting
+        {
+            get { return isConnecting; }
+            set
+            {
+                isConnecting = value;
+                if (isConnecting) StartedConnectingOn = DateTime.Now;
+                RaiseStartingConnectionChanged();
+            }
+        }
+        public bool IsConnected
+        {
+            get { return isConnected; }
+            set
+            {
+                isConnected = value;
+                if (isConnected)
+                {
+                    ConnectedOn = DateTime.Now;
+                }
+                else
+                {
+                    EndedOn = DateTime.Now;
+                }
+                RaiseConnectedChanged();
+            }
+        }
+        public bool IsOnHold
+        {
+            get { return isOnhold; }
+            set
+            {
+                isOnhold = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public ActiveCall()
+        {
+        }
+        public ActiveCall(NSUuid uuid, string handle, bool outgoing)
+        {
+            // Initialize
+            this.UUID = uuid;
+            this.Handle = handle;
+            this.IsOutgoing = outgoing;
+        }
+        #endregion
+
+        #region Public Methods
+        public void StartCall(ActiveCallbackDelegate completionHandler)
+        {
+            // Simulate the call starting successfully
+            completionHandler(true);
+
+            // Simulate making a starting and completing a connection
+            DispatchQueue.MainQueue.DispatchAfter(new DispatchTime(DispatchTime.Now, 3000), () => {
+                // Note that the call is starting
+                IsConnecting = true;
+
+                // Simulate pause before connecting
+                DispatchQueue.MainQueue.DispatchAfter(new DispatchTime(DispatchTime.Now, 1500), () => {
+                    // Note that the call has connected
+                    IsConnecting = false;
+                    IsConnected = true;
+                });
+            });
+        }
+
+        public void AnswerCall(ActiveCallbackDelegate completionHandler)
+        {
+            // Simulate the call being answered
+            IsConnected = true;
+            completionHandler(true);
+        }
+
+        public void EndCall(ActiveCallbackDelegate completionHandler)
+        {
+            // Simulate the call ending
+            IsConnected = false;
+            completionHandler(true);
+        }
+        #endregion
+
+        #region Events
+        public delegate void ActiveCallbackDelegate(bool successful);
+        public delegate void ActiveCallStateChangedDelegate(ActiveCall call);
+
+        public event ActiveCallStateChangedDelegate StartingConnectionChanged;
+        internal void RaiseStartingConnectionChanged()
+        {
+            StartingConnectionChanged?.Invoke(this);
+        }
+
+        public event ActiveCallStateChangedDelegate ConnectedChanged;
+        internal void RaiseConnectedChanged()
+        {
+            ConnectedChanged?.Invoke(this);
+        }
+        #endregion
+    }
+}
